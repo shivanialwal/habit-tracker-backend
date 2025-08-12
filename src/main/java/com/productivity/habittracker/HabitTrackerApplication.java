879d00e8a40db5bc7634b4d5f4909ab055a47fc7
SweetieAlwal <52544392+shivanialwal@users.noreply.github.com>
2025-08-12 15:29:17 +0530

    Third commit

--- src/main/java/com/productivity/habittracker/HabitTrackerApplication.java
+++ src/main/java/com/productivity/habittracker/HabitTrackerApplication.java
@@ -2,9 +2,12 @@ package com.productivity.habittracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class HabitTrackerApplication {
+@EnableJpaRepositories(basePackages = "com.productivity.habittracker.repository")
+@EntityScan(basePackages = "com.productivity.habittracker.model")public class HabitTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(HabitTrackerApplication.class, args);
     }
